00805490073555cb424a9874b5f66f377701cfe9
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-05-16 18:32:20 +0900

    Delete ex3 directory

--- ex3/Cow.java
+++
@@ -1,9 +0,0 @@
-package poly.ex3;
-
-public class Cow extends Animal{
-	
-	@Override
-	public void sound() {
-		System.out.println("음메");
-	}
-}
--- ex3/Dog.java
+++
@@ -1,9 +0,0 @@
-package poly.ex3;
-
-public class Dog extends Animal{ // 오버라이딩 전까지 오류가 뜬다 (컴파일 불가)
-	
-	@Override
-	public void sound() {
-		System.out.println("멍멍");
-	}
-}
