f7784108ab8fe5946f35f35ee4f3ad618480a9b0
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-05-30 17:43:41 +0900

    Add files via upload

---
+++ nullLabel/before/Label.java
@@ -0,0 +1,20 @@
+package nullLabelbefore;
+
+public class Label {
+
+    private String label;
+
+
+    public Label(String label) {
+        this.label = label;
+    }
+
+    public void display() {
+        System.out.println("display : " + label);
+    }
+
+    @Override
+    public String toString() {
+        return "\""+label+"\"";
+    }
+}
