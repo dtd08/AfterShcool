021df1a71f31a5d1d0b7b31832dfa14bf609a442
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-06-18 17:43:22 +0900

    Delete lang directory

--- lang/LangMain.java
+++
@@ -1,9 +0,0 @@
-package lang;
-
-//import java.lang.System;  import를 생략해도 문제 없음
-
-public class LangMain {
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
-    }
-}
--- lang/object/poly/Dog.java
+++
@@ -1,7 +0,0 @@
-package lang.object.poly;
-
-public class Dog {
-    public void sound() {
-        System.out.println("개는 '멍멍'하고 짖는다.");
-    }
-}
