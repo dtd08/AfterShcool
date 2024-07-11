72c47aa0edb45c366a9be1c635439edf8f195407
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-07-11 18:31:11 +0900

    Delete Lv 4/enumeration directory

--- Lv 4/enumeration/ex1/StringGrade.java
+++
@@ -1,9 +0,0 @@
-package enumeration.ex1;
-
-public class StringGrade { // 문자열 상수 이용
-    public static final String BASIC = "BASIC";
-    public static final String GOLD = "GOLD";
-    public static final String DIAMOND = "DIAMOND";
-
-
-}
