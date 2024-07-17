cf99274c218ae95236eea0dcae339b17be8b66c2
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-07-17 10:16:59 +0900

    Update Grade.java

--- Lv 4/enumeration/ex3/Grade.java
+++ Lv 4/enumeration/ex3/Grade.java
@@ -3,7 +3,6 @@ package enumeration.ex3;
 public enum Grade {
     BASIC, GOLD, DIAMOND
     // 끝. wow
-    // 전처럼 길게 쓸 필요도 없고
-    // 생성자를 고칠 필요도 없다 (생성자 안쓰임)
+    // 전처럼 길게 쓸 필요가 없다.
     // 홈쇼핑에 쓰이는 등급제도 이런식으로 된다.
 }
